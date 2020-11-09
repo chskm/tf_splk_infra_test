ea83e2835046fdead68bdbe4e14264a2cafef162
Sai Krishna <saikrishnamanohar@gmail.com>
2020-11-08 17:28:26 -0800

    removed keys from git

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.region
-  access_key = "AKIA3WYCSB4N7PIKWLWK"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
